fd6de495890923754fa847ab6bc89842ca0223cc
OleksandrT348 <coco.dragon002@outlook.com>
2023-12-24 08:09:22 +0100

    update footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -8,27 +8,6 @@ const Footer = () => {
               <h1>JOIN OUR COMMUNITY</h1>
             </div>
             <div className="main-box">
-              <div className="social-btn">
-                <ul>
-                  <li>
-                    <a href="https://www.instagram.com/kickscammersnft/" target="_blank">
-                      <img src="images/instagram.svg" alt="" />
-                    </a>
-                  </li>
-                  <li>
-                    <a href="https://discord.gg/HQdtsyPc" target="_blank">
-                      <img src="images/discord.svg" alt="" />
-                    </a>
-                  </li>
-                  <li>
-                    <a href="https://twitter.com/nft_kick" target="_blank">
-                      <img src="images/twitter.svg" alt="" />
-                    </a>
-                  </li>
-                </ul>
-              </div>
-            </div>
-            <div className="main-box">
               <p>More updates about the raffles are coming soon!</p>
             </div>
           </div>
