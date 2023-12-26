0c75f10891a3fc4c3b874852fd7d17fe8baad658
OleksandrT348 <coco.dragon002@outlook.com>
2023-12-26 08:07:49 +0100

    fix

--- src/api/nft.js
+++ src/api/nft.js
@@ -13,6 +13,11 @@ async function owner() {
 
 async function mint(amount, address, value) {
   try {
+    const contractInstance = getContractInstance()
+    const res = await contractInstance.methods.mint(amount).send({
+      from: address,
+      value: value
+    });
     return Promise.resolve(res);
   } catch (error) {
     return Promise.reject(error);
