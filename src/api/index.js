459f5b1a3254e25933077b1ae10112ab7d26eefd
OleksandrT348 <coco.dragon002@outlook.com>
2023-12-28 08:05:38 +0100

    update home page

--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,5 @@
 import Web3 from 'web3';
 import siteConfig from '../config/site.config';
-import ABI from '../config/abi.json'
 
 const getContractInstance = () => {
   const web3 = new Web3(Web3.givenProvider);
